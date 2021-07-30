81b6e33614bad096e8d76fd81f0aa60462bb05dd
Sergio Tovar <stovarm@gmail.com>
2021-07-30 15:49:09 -0500

    Enlace de HTML y Js, creando los inputs y configurando los resultados con innerText

--- descuentos.js
+++ descuentos.js
@@ -18,3 +18,18 @@ function calcularPrecioConDescuento(precio, descuento) {
     const precioConDescuento = (precio * procentajePrecioConDescuento)/100;
     return precioConDescuento;
 };
+
+function calculatePriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;  // Con innerText peudo escribir en HTML el resultado. enlazo con el mismo ID que defini en HTML
+    
+};
+
