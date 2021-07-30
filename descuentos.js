24eba21507bb00e3f3cce164759df9f293d34f1c
Sergio Tovar <stovarm@gmail.com>
2021-07-30 11:06:14 -0500

    Creación de la función para calcular precios y descuentos

--- descuentos.js
+++ descuentos.js
@@ -4,10 +4,17 @@ const descuento = 18;
 const procentajePrecioConDescuento = 100 - descuento;
 const precioConDescuento = (precioOriginal * procentajePrecioConDescuento)/100;
 
-console.log({          //Forma para imprimir como si fuera un objeto
-    precioOriginal, 
-    descuento,
-    procentajePrecioConDescuento,
-    precioConDescuento
-})
+// console.log({          //Forma para imprimir como si fuera un objeto
+//     precioOriginal, 
+//     descuento,
+//     procentajePrecioConDescuento,
+//     precioConDescuento
+// })
 
+// Segunda Parte del Taller - Crear la función.
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const procentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * procentajePrecioConDescuento)/100;
+    return precioConDescuento;
+};
