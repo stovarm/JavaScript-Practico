388233300167e7d76b9dc4eead71bce2c31ae787
Sergio Tovar <stovarm@gmail.com>
2021-07-31 10:35:26 -0500

    Cree la función calcular el promedio utilizando .reduce() en un array

--- promedio.js
+++ promedio.js
@@ -1,9 +1,18 @@
-function calcularMediaAritmetica(lista){
-    let sumaLista = 0;
+function calcularMediaAritmetica(lista) {
+    // let sumaLista = 0;
 
-    for (let i = 0; i < lista.length; i++) {              //.length es para identificar la cantidad de elementos del array
-        sumaLista = sumaLista + lista[i];  //comienzo en 0 y le suma el valor en la posición (i) luego el siguien pq i vale ++, y así sumo todos.
-    };
+    // for (let i = 0; i < lista.length; i++) {              //.length es para identificar la cantidad de elementos del array
+    //     sumaLista = sumaLista + lista[i];  //comienzo en 0 y le suma el valor en la posición (i) luego el siguien pq i vale ++, y así sumo todos.
+    // };
+    
+    const sumaLista = lista.reduce(            //permite sumar cada uno de los elementos. podemos cambiar la operación a -,/, * etc
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;    
+        }
+    ); 
+    
     const promedioLista = sumaLista / lista.length; 
-    return promedioLista
+
+    return promedioLista;
+
 };
