e3eea2bf911777243d63d55a669dab3f3fbc02be
Sergio Tovar <stovarm@gmail.com>
2021-07-31 11:18:22 -0500

    Converti todo el código en una funcion para calcular la mediana de cualquier lista

--- mediana.js
+++ mediana.js
@@ -1,29 +1,38 @@
-const lista1 = [
-    100,
-    200,
-    500,
-    400000,
-];
+function calcularMediaAritmetica(lista) {
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;    
+        }
+    ); 
+    
+    const promedioLista = sumaLista / lista.length; 
+    return promedioLista;
+};
 
+function calcularMediana (listaMediana) {
+    const mitadLista = parseInt(listaMediana.length/2);
+    let mediana;
 
-
-
-const mitadLista1 = parseInt(lista1.length/2);
-let mediana;
-
-function esPar(numerito) {
-    if(numerito % 2 === 0) {
-        return true;
-    } else {
-        return false;
+    function esPar(numerito) {
+        if(numerito % 2 === 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
 
-if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitadLista1];
-    const elemento2 = lista1[(mitadLista1-1)];
-    mediana = (elemento1 + elemento2) / 2;
-}
-else {
-    mediana = lista1[mitadLista1];
+    if (esPar(listaMediana.length)) {
+        const elemento1 = listaMediana[mitadLista-1];
+        const elemento2 = listaMediana[(mitadLista)];
+    
+        mediana = calcularMediaAritmetica([
+            elemento1,
+            elemento2,
+        ]);
+    }
+    else {
+        mediana = listaMediana[mitadLista];
+    }
+
+    return  mediana;
 }
