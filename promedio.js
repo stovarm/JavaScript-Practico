8e411672cc0b4cd7b6b3dc08b4512e80c65d682f
Sergio Tovar <stovarm@gmail.com>
2021-07-31 10:17:51 -0500

    Creación de la función para calcular el promedio de la lista

--- promedio.js
+++ promedio.js
@@ -1,14 +1,9 @@
-const lista1 = [
-    100,
-    200,
-    300,
-    500,
-];
+function calcularMediaAritmetica(lista){
+    let sumaLista = 0;
 
-let sumaLista1 = 0;
-
-for (let i = 0; i < lista1.length; i++) {              //.length es para identificar la cantidad de elementos del array
-    sumaLista1 = sumaLista1 + lista1[i];  //comienzo en 0 y le suma el valor en la posición (i) luego el siguien pq i vale ++, y así sumo todos.
+    for (let i = 0; i < lista.length; i++) {              //.length es para identificar la cantidad de elementos del array
+        sumaLista = sumaLista + lista[i];  //comienzo en 0 y le suma el valor en la posición (i) luego el siguien pq i vale ++, y así sumo todos.
+    };
+    const promedioLista = sumaLista / lista.length; 
+    return promedioLista
 };
-
-const promedioLista1 = sumaLista1 / lista1.length;
