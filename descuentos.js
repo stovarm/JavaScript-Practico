35508320190b784ff7c25adcd826f4f4e35159d7
Sergio Tovar <stovarm@gmail.com>
2021-07-31 09:36:14 -0500

    Se modifico el programa para recibir Coupones en vez de valores de dcto. Uso de switch y mensaje cuando el coupon no es válido

--- descuentos.js
+++ descuentos.js
@@ -1,8 +1,8 @@
-const precioOriginal = 120;
-const descuento = 18;
+// const precioOriginal = 120;
+// const descuento = 18;
 
-const procentajePrecioConDescuento = 100 - descuento;
-const precioConDescuento = (precioOriginal * procentajePrecioConDescuento)/100;
+// const procentajePrecioConDescuento = 100 - descuento;
+// const precioConDescuento = (precioOriginal * procentajePrecioConDescuento)/100;
 
 // console.log({          //Forma para imprimir como si fuera un objeto
 //     precioOriginal, 
@@ -23,13 +23,37 @@ function calculatePriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
 
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    let discountValue;
 
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;  // Con innerText peudo escribir en HTML el resultado. enlazo con el mismo ID que defini en HTML
-    
+    switch  (couponValue) {
+        case "SUPER15":
+            discountValue = 15;
+            break;
+        case "SUPER30":
+            discountValue = 30;
+            break;
+        case "SUPER50":
+            discountValue = 50;
+            break;
+        case "SUPER70":
+            discountValue = 70;
+            break; 
+        default:
+            discountValue = 0;
+            break;
+    };
+
+    if (discountValue == 0) { 
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El coupon no es válido, El precio se mantiene en: $" + priceValue;
+    }   
+        else {
+            const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+            const resultP = document.getElementById("ResultP");
+            resultP.innerText = "El precio con descuento son: $" + precioConDescuento;  // Con innerText peudo escribir en HTML el resultado. enlazo con el mismo ID que defini en HTML           
+        }        
 };
 
