112a81db45f3cd53228cf4be3215b44a63b4fe69
Sergio Tovar <stovarm@gmail.com>
2021-08-02 16:53:51 -0500

    Creación la lista de los salarios de las personas

---
+++ salarios.js
@@ -0,0 +1,85 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+colombia.push({
+    name: "Natalia",
+    salary: 60000,
+});
+colombia.push({
+    name: "Laura",
+    salary: 800,
+});
+colombia.push({
+    name: "Camila",
+    salary: 1500,
+});
+colombia.push({
+    name: "Juan Jose",
+    salary: 4000,
+});
+colombia.push({
+    name: "Sergio",
+    salary: 4500,
+});
+colombia.push({
+    name: "Isabel",
+    salary: 750,
+});
+colombia.push({
+    name: "Ervin",
+    salary: 2350,
+});
+colombia.push({
+    name: "Andres",
+    salary: 3300,
+});
+colombia.push({
+    name: "Jairo",
+    salary: 4100,
+});
+colombia.push({
+    name: "Monica",
+    salary: 3700,
+});
+colombia.push({
+    name: "Claudia",
+    salary: 2563,
+});
+colombia.push({
+    name: "Carlos",
+    salary: 2456,
+});
+colombia.push({
+    name: "Fredy",
+    salary: 2836,
+});
+colombia.push({
+    name: "Fernando",
+    salary: 3256,
+});
+colombia.push({
+    name: "Fernanda",
+    salary: 836,
+});
+colombia.push({
+    name: "Gregorio",
+    salary: 1346,
+});
+colombia.push({
+    name: "Frank",
+    salary: 2896,
+});
+colombia.push({
+    name: "Pedro",
+    salary: 3486,
+});
+colombia.push({
+    name: "Jimena",
+    salary: 3960,
+});
+colombia.push({
+    name: "Esteban",
+    salary: 4130,
+});
